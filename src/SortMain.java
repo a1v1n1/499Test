9a76338db71155b32026575d798bb255583d9935
a1v1n1 <qwertwatt@gmail.com>
2020-09-15 20:59:46 -0700

    removed comment

--- src/SortMain.java
+++ src/SortMain.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class SortMain {
 
-	public static void main(String[] args) {//comment
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Int or String Sort?");
 		String check = input.nextLine();
